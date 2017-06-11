a9748285e0df3be49c1e86d8e9712819c710087e
Steve Higgs <stevejhiggs@hotmail.com>
2017-06-11 15:36:09 +0100

    add linting tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
+const ts = require('gulp-typescript');
+const gulpTslint = require('gulp-tslint');
+const tslint = require('tslint');
 
 gulp.task('test', () => {
   gulp.src('tests/**/*.test.js')
@@ -9,11 +12,27 @@ gulp.task('test', () => {
     }));
 });
 
-gulp.task('lint', () =>
+gulp.task('eslint', () =>
   gulp.src(['src/**/*.js'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError())
 );
+
+gulp.task('tslint', () => {
+  const tsProject = ts.createProject('./tsconfig.json');
+  const program = tslint.Linter.createProgram('./tsconfig.json');
+
+  tsProject.src()
+    .pipe(gulpTslint({
+      formatter: 'stylish',
+      program
+    }))
+    .pipe(gulpTslint.report({
+      summarizeFailureOutput: true
+    }));
+});
+
+gulp.task('lint', ['eslint', 'tslint']);
 
 gulp.task('default', ['lint', 'test']);
